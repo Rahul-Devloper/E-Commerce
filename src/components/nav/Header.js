3dac37ff64d332128db1d79c728cc6c2e466eb82
Rahul-Devloper <rahulramesh26@yahoo.com>
2021-09-07 00:37:31 +0530

    integrated firebase signinwithEmail

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,39 +1,37 @@
 import React, { useState } from "react";
 import { Menu } from "antd";
+import { Link } from "react-router-dom";
 import {
-  MailOutlined,
-  AppstoreOutlined,
+  HomeOutlined,
   SettingOutlined,
+  LoginOutlined,
+  UserAddOutlined,
 } from "@ant-design/icons";
 
-const { SubMenu } = Menu;
-
-const handleClick = (e) => {
-  console.log(e);
-};
+const { SubMenu, Item } = Menu;
 
 const Header = () => {
-  const [current, setCurrent] = useState("");
-
+  const [current, setCurrent] = useState("home");
+  const handleClick = (e) => {
+    setCurrent(e.key);
+  };
   return (
     <div>
       <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-        <Menu.Item key="mail" icon={<MailOutlined />}>
-          Home
-        </Menu.Item>
-        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Register">
-          <Menu.Item key="setting:1">Option 1</Menu.Item>
-          <Menu.Item key="setting:2">Option 2</Menu.Item>
+        <Item key="home" icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Item>
+        <Item key="register" icon={<UserAddOutlined />}>
+          <Link to="/register">Register</Link>
+        </Item>
+        <Item key="login" icon={<LoginOutlined />}>
+          <Link to="/login">Login</Link>
+        </Item>
+
+        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
+          <Item key="setting:1">Option 1</Item>
+          <Item key="setting:2">Option 2</Item>
         </SubMenu>
-        <Menu.Item key="alipay">
-          <a
-            href="https://ant.design"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Navigation Four - Link
-          </a>
-        </Menu.Item>
       </Menu>
     </div>
   );
